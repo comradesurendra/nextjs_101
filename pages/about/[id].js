970a2a86c1f399b8dac835555f7187a9e1833597
comradesurendra <surendra10080@gmail.com>
2021-04-12 15:33:48 +0530

    Dynamic routing in nextjs

---
+++ pages/about/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DynamicPage() {
+    return (
+        <div>
+            <h1>Dynamic Page</h1>
+        </div>
+    )
+}
+
+export default DynamicPage
---
+++ pages/portpholio/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Portpholio() {
+    return (
+        <div>
+            <h1>The Portpholio Page</h1>
+        </div>
+    )
+}
+
+export default Portpholio
